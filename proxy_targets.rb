9a0af1f32ffb34c3f1ad060a57c71ac3124df537
Nicolay Hvidsten <nico.hvi@gmail.com>
2014-10-05 15:56:57 +0200

    added host to ruby script

--- proxy_targets.rb
+++ proxy_targets.rb
@@ -3,25 +3,28 @@ require 'net/http'
 require 'yaml'
 
 # find all linked containers through environment variables
-apps = {}
+apps = { }
 
 ENV.each do |env, value|
   env_parts = env.split('_')
   
   # custom environment variables added through docker run -e
   if env_parts[0] == 'APP' 
-    p "this is app env: #{env_parts}"
-    # apps[container] ||= { }
-    
-    # env_parts[0] == 'APP' && env_parts[2] == 'PORT'
-    #container = env_parts[1].downcase
-    # = value.sub(/^.*?:\/\//, '').split(':')
-    #apps[container] = {}
-    #apps[container][:ip] = ip_port[0]
-    #apps[container][:port] = ip_port[1]
-  end
+    app = env_parts[1]
+    apps[app] ||= { }
+    key = env_parts[2]
+    if key == 'PORT' 
+      # remove tcp:// prefix and split on port number
+      ip_port = value.sub(/^.*?:\/\//, '').split(':') 
+      apps[app][:ip] = ip_port[0]
+      apps[app][:port] = ip_port[1]
+    elsif key == 'HOST'
+      apps[app][:host] = value
+    end
+  end 
 end
 
+p apps
 # copy default nginx site conf template and replace tokens
 
 #apps.each do |container, app|
